ebf3b51808be3298a78631e7fa05f5015a27c13d
xilinshen <57839020+xilinshen@users.noreply.github.com>
2022-06-23 11:28:09 +0800

    Update and rename train_cellline.sh to train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 python main.py \
 --file "./data/splatter_simulation.npz" \
 --arch densenet21 \
---batch-size 32 \
+--batch-size 128 \
 --dist-url "tcp://localhost:10006" \
---outdir "./cellline_unscale/" \
+--outdir "./result/" \
 --mlp --moco-k 448 --moco-m 0.999 \
---in_features 1959 \
---num_batches 3 \
+--in_features 2000 \
+--num_batches 5 \
 --shuffle-ratio 0.1 \
 --randomzero-ratio 0.3 \
 --epochs 100 \
